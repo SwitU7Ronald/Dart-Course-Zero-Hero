a78d5989e4a9d5f1d071529c5ec3b385cb575090
SwitU7Ronald <c2tu6u@gmail.com>
2025-07-30 21:54:29 +0530

    Fix pubspec Dependencies

--- 12_json/json_serializable_example.dart
+++ 12_json/json_serializable_example.dart
@@ -1,11 +1,11 @@
-// You need build_runner and json_serializable packages
-// Run: dart run build_runner build
-
-import 'dart:convert';
+// Import json_annotation package for annotations
 import 'package:json_annotation/json_annotation.dart';
+import 'dart:convert';
 
+// This tells build_runner where to generate the part file
 part 'json_serializable_example.g.dart';
 
+// Annotate your class with @JsonSerializable
 @JsonSerializable()
 class User {
   final String name;
@@ -13,19 +13,21 @@ class User {
 
   User({required this.name, required this.score});
 
+  // A factory constructor for creating a new User instance from a map
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
+  // A method to convert User instance into a map
   Map<String, dynamic> toJson() => _$UserToJson(this);
 }
 
 void main() {
   String jsonStr = '{"name": "Raees", "score": 42}';
 
-  // Deserialize
+  // Deserialize JSON string to User object
   User user = User.fromJson(jsonDecode(jsonStr));
   print('🙋 Name: ${user.name}, 🧮 Score: ${user.score}');
 
-  // Serialize
+  // Serialize User object to JSON string
   String encoded = jsonEncode(user.toJson());
   print('📦 JSON: $encoded');
 }
